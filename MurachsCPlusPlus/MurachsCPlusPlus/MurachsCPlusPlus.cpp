e021be4feb17152e3d9832807a2185e9e6630b72
rwmerrigan <76187538+rwmerrigan@users.noreply.github.com>
2021-04-09 11:46:49 -0700

    Random number and guest book

--- MurachsCPlusPlus/MurachsCPlusPlus/MurachsCPlusPlus.cpp
+++ MurachsCPlusPlus/MurachsCPlusPlus/MurachsCPlusPlus.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::pow;
 using std::round;
+using namespace std;
 
 int main()
 {
@@ -73,6 +77,53 @@ int main()
          << "Bye!:          ";
     */
    
+    //Random Number die throw
+    /*
+    //use time() to get an int value
+    int elapsed_seconds = time(nullptr);
+
+    // seed the random number generator
+    srand(elapsed_seconds);
+
+    //roll the first die
+    int die1 = rand() % 6; //die1 is >= 0 and < 6
+    die1 = die1 + 1;       //die2 is >= 1 and <= 6
+
+    //roll the second die
+    int die2 = rand() % 6; //die2 is >= 0 and < 6
+    die2 = die2 + 1;       //die2 is >= 1 and <= 6
+
+    cout << "Your roll:  " << die1 << " " << die2;
+    */
+    
+    //Guest Book
+    cout << "Guest Book\n\n";
+
+    string first_name;
+    cout << "First name:\t";
+    cin >> first_name;            //get first string only
+    cin.ignore(100, '\n');        //discard leftover chars and newline
+    
+    char middle_initial;
+    cout << "Middle initial:\t";
+    middle_initial = cin.get();   //get first char only
+    cin.ignore(100, '\n');        //discard leftover chars and newline
+
+    string last_name;
+    cout << "Last name:\t";
+    getline(cin, last_name);      //get entire line
+
+    string city;
+    cout << "City:\t\t";
+    getline(cin, city);           //get entire line
+
+    string country;
+    cout << "Country:\t";
+    getline(cin, country);        //get entire line
+
+    cout << "\nENTRY\n"           //display the entry
+        << first_name + ' ' + middle_initial + ". " + last_name + '\n'
+        << city + ", " + country + "\n\n";
     
 }
 
--- MurachsCPlusPlus/MurachsCPlusPlus/README.md
+++ MurachsCPlusPlus/MurachsCPlusPlus/README.md
@@ -3,4 +3,5 @@ Murach's C++ Notes
 Ch. 1 
 - #include statements show the header files being used, called preprocessor directives
 - namespace provides a way to organize code to avoid naming conflicts
-- 
+
+Ch 2
