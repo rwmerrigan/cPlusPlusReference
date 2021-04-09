4f7fa421fd16e275eae6ad9b1104bd30c821e39c
rwmerrigan <76187538+rwmerrigan@users.noreply.github.com>
2021-04-08 17:36:33 -0700

    Added random programs from chapter 1

---
+++ MurachsCPlusPlus/MurachsCPlusPlus/MurachsCPlusPlus.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::pow;
+using std::round;
+
+int main()
+{
+    //Calorie Calculator
+    /*
+    cout << "Welcome to the calorie Calculator\n\n";
+
+    //get number of servings from user
+    double servings;
+    cout << "Enter servings per food item: ";
+    cin >> servings;
+
+    //get number of calories from user
+    double calories;
+    cout << "Enter calories per serving: ";
+    cin >> calories;
+
+    //calculate total calories
+    double total_calories = servings * calories;
+
+    //display total calories
+    cout << "Total Calories: " << total_calories << endl << endl;
+    */
+
+    //Gallon to Liters
+    /*
+    * //print name of program
+    cout << "Gallons Converter" << endl << endl;
+
+    //get gallons from user
+    double gallons;
+    cout << "Enter gallons: ";
+    cin >> gallons;
+
+    //convert gallons to liters
+    double liters = gallons * 3.7854;
+
+    //write output to console
+    cout << "Liters:    " << liters << endl << endl << "Bye!" << endl << endl;
+    */
+    
+    //Circle Calculator
+    /*
+     cout << "Circle Calculator" << endl << endl;
+
+    //get radius from user
+    double radius;
+    cout << "Enter radius: ";
+    cin >> radius;
+
+    //make calculations
+    double pi = 3.14159;
+    double diameter = 2 * radius;
+    double circumference = diameter * pi;
+    double area = pi * pow(radius, 2.0);
+
+    //round to 1 decimal place
+    circumference = round(circumference * 10) / 10;
+    area = round(area * 10) / 10;
+
+    //write output to console
+    cout << "Diameter:      " << diameter << endl
+         << "Circumference: " << circumference << endl
+         << "Area:          " << area << endl
+         << "Bye!:          ";
+    */
+   
+    
+}
+
---
+++ MurachsCPlusPlus/MurachsCPlusPlus/README.md
@@ -0,0 +1,6 @@
+Murach's C++ Notes
+
+Ch. 1 
+- #include statements show the header files being used, called preprocessor directives
+- namespace provides a way to organize code to avoid naming conflicts
+- 
